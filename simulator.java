0fd02bc1b660c7050a509c63b3fcee148ec37a26
Josephine Søgaard Andersen <josea18@student.sdu.dk>
2018-10-25 14:43:55 +0200

    Edited Simulator.java

--- simulator.java
+++ simulator.java
@@ -1,22 +1,23 @@
 public class Simulator{
-public static void main(String[] args){
+	
+	public static void main(String[] args){
 
-City copenhagen=new City("Copenhagen",10,20);
+		City copenhagen=new City("Copenhagen",10,20);
 
-City berlin=new City("berlin",100,200);
+		City berlin=new City("Berlin",100,200);
 
-System.out.println(copenhagen.name());
-System.out.println(copenhagen.x());
-System.out.println(copenhagen.y());
+		System.out.println(copenhagen.name());
+		System.out.println(copenhagen.x());
+		System.out.println(copenhagen.y());
 
-System.out.println(berlin.name());
-System.out.println(berlin.x());
-System.out.println(berlin.y());
-
-
-
+		System.out.println(berlin.name());
+		System.out.println(berlin.x());
+		System.out.println(berlin.y());
 
 
-}
+
+
+
+	}
 
 }
