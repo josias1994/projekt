414dd4f2f5be90ed593ff90493f71bd03eecf35b
josias <Josiaskure@gmail.com>
2018-10-25 14:35:05 +0200

    Uploaded

---
+++ simulator.java
@@ -0,0 +1,22 @@
+public class Simulator{
+public static void main(String[] args){
+
+City copenhagen=new City("Copenhagen",10,20);
+
+City berlin=new City("berlin",100,200);
+
+System.out.println(copenhagen.name());
+System.out.println(copenhagen.x());
+System.out.println(copenhagen.y());
+
+System.out.println(berlin.name());
+System.out.println(berlin.x());
+System.out.println(berlin.y());
+
+
+
+
+
+}
+
+}
