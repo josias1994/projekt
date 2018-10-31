65758de7226f675ef9d0595b5e08f2a48827dfac
Pennek <Pauel@protonmail.com>
2018-10-31 13:53:55 +0100

    Added userinput, EventQueue & Population initialization.

--- simulator.java
+++ simulator.java
@@ -1,44 +1,37 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Simulator{
 
 	public static void main(String[] args){
+		int initPop, maxPop, pDeath, pMutate, pRepro;
+		double pOmega;
+
 		Scanner reader = new Scanner(System.in);
+		System.out.println("What is your initial population?");
+		initPop = reader.nextInt();
+		System.out.println("What is your maximum population?");
+		maxPop = reader.nextInt();
+		System.out.println("What is the value of the parameter Omega?");
+		pOmega = reader.nextInt();
+		System.out.println("What is the value of the parameter D?");
+		pDeath = reader.nextInt();
+		System.out.println("What is the value of the parameter M?");
+		pMutate = reader.nextInt();
+		System.out.println("What is the value of the parameter R?");
+		pRepro = reader.nextInt();
 
 		/*
-		*	Get parameters from userinput.
+		*	Main loop to interact with EventQueue.class and Population.class
 		*/
-		System.out.println("What is your initial population?");
-		int initPop = reader.nextInt();
-		System.out.println("What is your maximum population?");
-		int maxPop = reader.nextInt();
-		System.out.println("What is the value of the parameter Omega?");
-		double pOmega = reader.nextInt();
-		System.out.println("What is the value of the parameter D?");
-		int pDeath = reader.nextInt();
-		System.out.println("What is the value of the parameter M?");
-		int pMutate = reader.nextInt();
-		System.out.println("What is the value of the parameter R?");
-		int pRepro = reader.nextInt();
-
-
-
-		City copenhagen=new City("Copenhagen",10,20);
-
-		City berlin=new City("Berlin",100,200);
-
-		System.out.println(copenhagen.name());
-		System.out.println(copenhagen.x());
-		System.out.println(copenhagen.y());
-
-		System.out.println(berlin.name());
-		System.out.println(berlin.x());
-		System.out.println(berlin.y());
-
-
-
-
-
+		EventQueue queue = new EventQueue();
+		Population population = new Population(pOmega);
+		System.out.println("");
 	}
 
+	/*
+	*	Get parameters from userinput.
+	*/
+	public static void GetInput(){
+
+	}
 }
