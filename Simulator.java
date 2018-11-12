f17bc5ad94d965780cd9f0972caebb0d53c60394
Pennek <Pauel@protonmail.com>
2018-11-12 07:45:15 +0100

    Creation of Inital Population + Events

--- Simulator.java
+++ Simulator.java
@@ -6,10 +6,10 @@ import java.util.*;
 public class Simulator{
 	private static int initPop, maxPop, pDeath, pMutate, pRepro;
 	private static Scanner Reader;
-	private static City[] cities;
-	private static Population pop;
-	public static double pOmega;
-	public static final char DEATH='d';
+	private static double pOmega;
+	private static final char DEATH='d', MUTATION='m', REPRODUCTION='r';
+	private static Population pop = new Population(pOmega);
+	private static EventQueue eventQueue = new EventQueue();
 
 
 
@@ -30,32 +30,45 @@ public class Simulator{
 		System.out.println("What is the value of the parameter R?");
 		pRepro = Reader.nextInt();
 	}
-	
-	/*
-	* Constructors
-	*/
-	CityGenerator cityGenerator = new CityGenerator();
-	EventQueue eventQueue = new EventQueue();
 
 	public static void main(String[] args){
 		Reader = new Scanner(System.in);
 		init();
-		boolean goOn = true;
-		while(goOn){
-			goOn = false;
-		}
 
-		City[] cities = new CityGenerator().generate();
-		Population pop = new Population(pOmega);
+		City[] currentCities = new CityGenerator().generate();
+		createInitialPopulation(currentCities);
 
-		int i = 0;
-		while(i<initPop){
-			Individual k = new Individual(cities);
-			pop.add(k);
-			i=i+1;
-		}
 
-		System.out.println("Population Size: " + pop.size());
 		Reader.close();
 	}
+
+	/*
+	*	Creates the Initial Population and the Events following
+	*/
+	private static void createInitialPopulation(City[] currentCities){
+		int i = 0;
+		while(i<initPop){
+			Individual person = new Individual(currentCities);
+			pop.add(person);
+			createEvents(person);
+			i=i+1;
+		}
+	}
+
+	/*
+	*	Adds a Mutation-, Reproduction- and Death-event to the EventQueue, for the specified Individual
+	*/
+	private static void createEvents(Individual person){
+		double mTime = new RandomUtils().getRandomTime((1-(Math.log(pop.fitness(person)))) * pMutate);
+		double rTime = new RandomUtils().getRandomTime((1-(Math.log(pop.fitness(person)))) * (initPop/maxPop) * pRepro);
+		double dTime = new RandomUtils().getRandomTime((1-(Math.log(1 - pop.fitness(person)))) * pDeath);
+		Event mutationEvent = new Event(MUTATION, mTime, person);
+		Event reproductionEvent = new Event(REPRODUCTION, mTime, person);
+		Event deathEvent = new Event(DEATH, mTime, person);
+		eventQueue.add(mutationEvent);
+		eventQueue.add(reproductionEvent);
+		eventQueue.add(deathEvent);
+	}
+
+
 }
